492e529d23d086b7c913ed04a77739c0b548ec26
Alex <alexandros8a8@gmail.com>
2020-02-28 13:59:27 +0200

    Initial commit

---
+++ app/src/main/java/com/alexchar_dev/socialrelationships/presentation/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alexchar_dev.socialrelationships.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexchar_dev.socialrelationships.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
